37895da34243237a373c70f4ecf8a9b85b4f3693
David Brodrick <128331752+davedevcodes@users.noreply.github.com>
2024-12-24 09:17:08 +0100

    Update RequestPage.tsx

--- pages/RequestPage.tsx
+++ pages/RequestPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { ThemeProvider, useTheme } from '../context/ThemeContext'
+import { ThemeProvider } from '../context/ThemeContext'
 import NavBar from '@/components/navbar';
 import Top from '@/components/main/Top';
 import Table from '@/components/requests/Table';
 import Footer from '@/components/Footer';
 import '../app/globals.css';
 import '../components/styles/request.css';
-import { GetStaticProps } from 'next';
 
 const RequestPage = () => {
 
