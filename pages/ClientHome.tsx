97e7975ff270aaecad21298ed8c74069d9b0952d
David Brodrick <128331752+davedevcodes@users.noreply.github.com>
2024-12-24 09:11:56 +0100

    Update ClientHome.tsx

--- pages/ClientHome.tsx
+++ pages/ClientHome.tsx
@@ -2,11 +2,10 @@ import ClientNavBar from "@/components/clients/ClientNavBar";
 import Footer from "../components/Footer";
 import ClientTop from "@/components/clients/ClientTop";
 import ClientTable from "@/components/clients/ClientTable";
-import QuoteForm from "@/components/clients/QuoteForm";
 import '../app/globals.css';
 import '../components/styles/request.css'
 import React from 'react'
-import { ThemeProvider, useTheme } from '../context/ThemeContext'
+import { ThemeProvider } from '../context/ThemeContext'
 
 const ClientHome = () => {
   return (
